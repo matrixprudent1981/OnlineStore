081f8b5cf32392c813fe77846f8ed4d04a0b6815
Nurbek <matrix.prudent1981@gmail.com>
2023-10-26 20:14:17 +0600

    add model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, PostTest, Hashtag
 
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(PostTest)
+admin.site.register(Hashtag)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from blog.constans import TYPE_BOOK
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name="Заголовок")
@@ -15,6 +17,7 @@ class Post(models.Model):
         verbose_name = 'Пост'
         verbose_name_plural = "Посты"
 
+
 class Comment(models.Model):
     text = models.TextField(max_length=200, verbose_name="коментарий")
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -25,3 +28,40 @@ class Comment(models.Model):
     class Meta:
         verbose_name = 'Коментарий'
         verbose_name_plural = "Коментарии"
+
+
+class Hashtag(models.Model):
+    title = models.CharField(max_length=64)
+    titl2 = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.title} - {self.titl2}"
+
+    class Meta:
+        verbose_name = "Хештег"
+        verbose_name_plural = 'Хештеги'
+
+
+class PostTest(models.Model):
+    ''' References'''
+
+    hashtags = models.ManyToManyField(Hashtag)
+
+    ''' Base field '''
+
+    title = models.CharField(max_length=400, null=True)
+    image = models.ImageField(upload_to='media/', null=True)
+    description = models.TextField(null=True)
+    tipe_book = models.CharField(max_length=100, choices=TYPE_BOOK, null=True)
+    cost = models.PositiveIntegerField(null=True)
+    director = models.CharField(max_length=100, null=True)
+    number_of_page = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return (f'Название книги {self.title}\n'
+                f'Цена книги {self.cost}')
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
